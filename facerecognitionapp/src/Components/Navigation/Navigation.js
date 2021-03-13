12a07510663bd3d9c1ea4d61373e289adf895f90
dhruv-bhatia246 <71057049+dhruv-bhatia246@users.noreply.github.com>
2021-03-13 23:47:55 +0530

    Add files via upload

---
+++ facerecognitionapp/src/Components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'tachyons';
+
+const Navigation = ({onRouteChange,route,isSignedIn}) => {
+    if(isSignedIn)
+    return(
+        <nav style={{display:'flex',justifyContent:'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+    )
+    else {
+        if(route==='register')
+        return(
+            <nav style={{display:'flex',justifyContent:'flex-end'}}>
+                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+            </nav>
+        )
+        else
+        return(
+            <nav style={{display:'flex',justifyContent:'flex-end'}}>
+                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+            </nav>
+        )
+    }
+}
+
+export default Navigation;
