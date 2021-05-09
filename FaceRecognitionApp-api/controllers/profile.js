63b689dc69b55161f48a17fa50f7cb8e1396c8cf
dhruv-bhatia246 <71057049+dhruv-bhatia246@users.noreply.github.com>
2021-05-10 04:03:03 +0530

    Add files via upload

---
+++ FaceRecognitionApp-api/controllers/profile.js
@@ -0,0 +1,21 @@
+const handleProfile = (req,res,db) => {
+    const { id } = req.params;
+    let found = false;
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user=>{
+        console.log(user);
+        if(user.length){
+            res.json(user[0]);
+        }
+        else{
+            res.status(400).json('Not Found')
+        }
+    })
+    .catch(err => res.status(400).json('Not Found'));
+}
+
+module.exports = {
+    handleProfile: handleProfile
+};
